b6868f1cf5308d11792f4471cb3ff64cfaa0503f
kre8mymedia <kre8mymedia@gmail.com>
2023-12-03 18:48:22 -0600

    Will use loader from lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,26 +1,37 @@
-import os
-import sys
-import json
-import requests
-
 from promptengineers.repos.user import UserRepo
+from promptengineers.controllers.retrieval import accumulate_loaders
+from promptengineers.services.pinecone import PineconeService
+from langchain.embeddings.openai import OpenAIEmbeddings
 
 APP_ENV = 'development'
 
 def lambda_handler(event, context):
     # TODO implement
     pinecone_keys = ['PINECONE_KEY', 'PINECONE_ENV', 'PINECONE_INDEX', 'OPENAI_API_KEY']
+    loaders = accumulate_loaders(event.get('data'))
     tokens = UserRepo().find_token(
         event.get('user_id'),
         pinecone_keys
     )
+    embeddings = OpenAIEmbeddings(openai_api_key=tokens.get('OPENAI_API_KEY'))
+    pinecone_service = PineconeService(
+        api_key=tokens.get('PINECONE_KEY'),
+        env=tokens.get('PINECONE_ENV'),
+        index_name=tokens.get('PINECONE_INDEX'),
+    )
+    result = pinecone_service.from_documents(
+        loaders,
+        embeddings,
+        namespace=event.get('data').get('index_name')
+    )
+    
 
     # Checking if the request was successful
-    if tokens:
+    if result:
         # Parsing the JSON response
         return {
             'statusCode': 200,
-            'body': tokens
+            'data': f"Index [{event.get('data').get('index_name')}] created successfully"
         }
     else:
         print(f"Failed to retrieve data")
@@ -30,7 +41,16 @@ def lambda_handler(event, context):
 
 if __name__ == '__main__' and APP_ENV == 'development':
     event = {
-        'user_id': '000000000000000000000000'
+        "user_id": "000000000000000000000000",
+        "data": {
+            "index_name": "test-index",
+            "loaders": [
+                {
+                    "type": "copy",
+                    "text": "In a quiet village, a young girl discovered a mysterious, glowing stone in the forest. Each night, the stone guided her to help people in secret, solving problems while everyone slept. Over time, the village flourished, and stories of an invisible guardian spread far and wide."
+                }
+            ]
+        }
     }
 
     context = {}
